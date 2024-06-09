ae2f991f37afa50623293291e5246d8440c2dad3
Rowland Luo <rowlandluo0713@gmail.com>
2024-06-09 17:31:22 +0100

    Generated a new dataframe with warmth and hostility discrepancy for paternal data. I also added my comments on how to interpretate the data and teh data distribution.

--- SchoolTransition.R
+++ SchoolTransition.R
@@ -95,7 +95,37 @@ PaternalPupils %in% PaternalPWHdata$ID
 #So no, not all children with their dad answering the questionnaire had their paternal PWH data. So now we need to filter out those who had their paternal PWH data.
 list <- as.list(PaternalPWHdata$ID)
 commonlist <- as.list(intersect(unlist(PaternalPupils), unlist(list)))
-newdata <- PaternalPWHdata %>%
+newdata_childperceived <- PaternalPWHdata %>%
   filter(ID %in% commonlist) %>%
   select(ID, hostility, warmth)
-  
+newdata_childperceived
+#This is the list of child-perceived parental warmth that can be compared with father-perceived parental warmth. 
+Father_PWHdata_overall <- Parent_PWHdata_overall %>%
+  filter(P1_ParentGender == 1)
+#This is the list of parent-perceived parental warmth (dad). Now we check if all IDs in the first list is exactly the same as the IDs in the second list.
+newdata_childperceived$ID %in% Father_PWHdata_overall$ID
+PaternalDataset1 <- merge(newdata_childperceived, Father_PWHdata_overall) %>%
+  as_tibble
+PaternalDataset1 <- PaternalDataset1 %>%
+  rename( hostility_C1 = hostility,
+        warmth_C1 = warmth,
+        hostility_P1 = hostility_P,
+        warmth_P1 = warmth_P)
+PaternalDataset1 <- PaternalDataset1 %>%
+  select(ID, hostility_C1, warmth_C1, hostility_P1, warmth_P1) %>%
+  mutate (hostility_Discrepancies = hostility_C1 - hostility_P1) %>%
+  mutate(warmth_Discrepancies = warmth_C1 - warmth_P1)
+#Now we explore the distribution of the paternal discrepancies of warmth and hostility.
+p5 <- ggplot(PaternalDataset1, aes(x = hostility_Discrepancies)) +
+  geom_histogram(aes(y = ..density..),
+                 binwidth = 2,
+                 colour = "black", fill = "white")+
+  geom_density(alpha = .2, fill="#FF6")
+p6 <- ggplot(PaternalDataset1, aes(x = warmth_Discrepancies)) +
+  geom_histogram(aes(y = ..density..),
+                 binwidth = 2,
+                 colour = "black", fill = "white")+
+  geom_density(alpha = .2, fill="#FF6666")
+p_density3 <- grid.arrange(p5, p6)
+#We shall pause and do some interpretations here: Lower number means more: for example, a lower number of warmth means that there is MORE warmth in the parental relationship; similarly, a high number of hostility means that there is LESS hostility in the parental relationship. So if the hostility discrepancy is a large positive number, it means that the child has a higher perceived hostility score, meaning that the child thinks that the parent is being LESS hostile than the parents think they are. On the other hand, if the hostility discrepancy is a large negative number, it means that the child has a lower perceived hostility score, meaning that the child thinks that the parent is being MORE hostile than the parents thinks they are. In similar senses, a large positive warmth discrepancy number means that the child has a higher perceived warmth score, meaning that the child thinks that the parent is being LESS warm than the parents think they are. A large negative warmth discrepancy number means that the child has a lower perceived warmth score, meaning that the child thinks that the parent is being MORE warm than the parents think they are.
+#We can observe from the distribution shape that hostility discrepancy is left skewed while warmth discrepancy is right skewed. And this means that parents generally tend to think they are more hostile and less warm than the children think they are. This could be a demonstration of parental guilt---parents sometime tend to over-reflect on their parental behaviors. Note that this is only the paternal data though. The next step is to see if similar patterns show on maternal data.
