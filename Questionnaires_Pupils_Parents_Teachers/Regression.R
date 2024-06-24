efc69d54d9cd4bbe0067a6842f08913c920ff2a4
hotchocomel <156305918+hotchocomel@users.noreply.github.com>
2024-06-24 13:36:37 +0100

    Create Regression.R

---
+++ Questionnaires_Pupils_Parents_Teachers/Regression.R
@@ -0,0 +1,52 @@
+#regression 
+children_difficult_wave1 <- schooltransitiondata %>% select(1, 98:122)
+children_difficult_wave1_filtered <- children_difficult_wave1 %>% filter(complete.cases(.))
+children_difficult_wave1_filtered_sum <- children_difficult_wave1_filtered %>% 
+  rowwise() %>%
+  mutate(total_score_wave1 = sum(c_across(2:26), na.rm = TRUE)) %>%
+  ungroup()
+
+children_difficult_wave2 <- schooltransitiondata %>% select(1, 575:599)
+children_difficult_wave2_filtered <- children_difficult_wave2 %>% filter(complete.cases(.))
+children_difficult_wave2_filtered_sum <- children_difficult_wave2_filtered %>% 
+  rowwise() %>%
+  mutate(total_score_wave2 = sum(c_across(2:26), na.rm = TRUE)) %>%
+  ungroup()
+
+children_difficult_merge <- merge(children_difficult_wave1_filtered_sum, children_difficult_wave2_filtered_sum, by = "ID", all = FALSE)
+
+#check the difficult score distribution a bit
+ggplot(children_difficult_merge, aes(x = total_score_wave1)) +
+  geom_histogram(aes(y = ..density..), bins = 10, fill = "skyblue", color = "black", alpha = 0.7) +
+  stat_function(fun = dnorm, args = list(mean = mean(children_difficult_merge$total_score_wave1, na.rm = TRUE), 
+                                        sd = sd(children_difficult_merge$total_score_wave1, na.rm = TRUE)),
+                color = "red", size = 1) +
+  labs(title = "Distribution of Wave 1 Difficult Score", x = "Total Score", y = "Density") +
+  theme_minimal()
+
+ggplot(children_difficult_merge, aes(x = total_score_wave2)) +
+  geom_histogram(aes(y = ..density..), bins = 10, fill = "skyblue", color = "black", alpha = 0.7) +
+  stat_function(fun = dnorm, args = list(mean = mean(children_difficult_merge$total_score_wave2, na.rm = TRUE), 
+                                        sd = sd(children_difficult_merge$total_score_wave2, na.rm = TRUE)),
+                color = "red", size = 1) +
+  labs(title = "Distribution of Wave 2 Difficult Score", x = "Total Score", y = "Density") +
+  theme_minimal()
+
+#back to work
+discrepancy_score <- rbind(MaternalDataset1, PaternalDataset1)
+regression_dataframe <- merge(children_difficult_merge, discrepancy_score, by = "ID", all = FALSE)
+
+install.packages("lavaan")
+library(lavaan)
+
+model_warmth <- '
+total_score_wave2 ~ warmth_Discrepancies + total_score_wave1
+'
+fit_warmth <- sem(model_warmth, data = regression_dataframe)
+summary(fit_warmth)
+
+model_hostility <- '
+total_score_wave2 ~ hostility_Discrepancies + total_score_wave1
+'
+fit_hostility <- sem(model_hostility, data = regression_dataframe)
+summary(fit_hostility)
